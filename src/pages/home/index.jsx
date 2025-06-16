e112d82d5d9f4ab7177025256241218701180d38
wallissl <wallis.sl@outlook.com>
2025-06-16 20:33:09 -0300

    criação do button

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,15 @@
+import { Button } from "../../components/button";
+import { NewCycle } from "../../components/new-cycle";
 import { Timer } from "../../components/timer";
+import './home.css';
 
 export function HomePage() {
     return (
 
-        <div>
+        <div className="container--home"> 
+            <NewCycle   />
             <Timer />
+            <Button>Começar</Button>
                 
         </div>
         
