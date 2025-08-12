4e2fae4af919075768ffcb92f97ac92aee2f9a76
wallissl <wallis.sl@outlook.com>
2025-08-11 21:33:50 -0300

    up

--- src/components/new-cycle/index.jsx
+++ src/components/new-cycle/index.jsx
@@ -1,14 +1,16 @@
+import { useFormContext } from 'react-hook-form'
 import './new-cycle.css'
-export function NewCycle() {
 
+export function NewCycle() {
+    const { register } = useFormContext();
     return (
         <div className='container--new-cycle'>
         
             <label htmlFor="task">Vou trabalhar em:</label>
-            <input type="text" id="task" placeholder='Criar timer'/>
+            <input type="text" id="task" placeholder='Criar timer' {...register('task')}/>
 
             <label htmlFor="minutesAmount">durante</label>
-            <input type="number" id="minutesAmount" />
+            <input type="number" id="minutesAmount" {...register('minutesAmount', { valueAsNumber: true })}/>
 
             <span>minutos.</span>
             
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,20 +2,27 @@ import { Button } from "../../components/button";
 import { NewCycle } from "../../components/new-cycle";
 import { Timer } from "../../components/timer";
 import './home.css';
-import { userForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 
 
 export function HomePage() {
 
-    const { register, handleSubmit } = useForm()
+    const methods = useForm();
+    const { handleSubmit} = methods; 
+
+    function createNewCycle(data){
+        console.log(data)
+    }
 
     return (
 
-        <form className="container--home"> 
-            <NewCycle   />
-            <Timer />
-            {/* <Button variant="secondary">Começar</Button> */}
+        <form className="container--home" onSubmit={handleSubmit(createNewCycle)}>
+            {/* <input type="text" {...register('name', { required: true })} /> // input para o nome do ciclo */}
 
+            <FormProvider {...methods}>
+            <NewCycle />
+            </FormProvider>
+            <Timer />
             <Button>Começar</Button>
                 
         </form>
